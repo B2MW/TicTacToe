a5cc8d0550c42ced3208fb0c4e4ce5fd6f76cf81
B2MW <P5@Champion.local>
2014-10-02 17:16:34 -0500

    Label Updates

--- TicTacToe/ViewController.m
+++ TicTacToe/ViewController.m
@@ -9,7 +9,16 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+@property (strong, nonatomic) IBOutlet UILabel *labelOne;
+@property (strong, nonatomic) IBOutlet UILabel *labelTwo;
+@property (strong, nonatomic) IBOutlet UILabel *labelThree;
+@property (strong, nonatomic) IBOutlet UILabel *labelFour;
+@property (strong, nonatomic) IBOutlet UILabel *labelFive;
+@property (strong, nonatomic) IBOutlet UILabel *labelSix;
+@property (strong, nonatomic) IBOutlet UILabel *labelSeven;
+@property (strong, nonatomic) IBOutlet UILabel *labelEight;
+@property (strong, nonatomic) IBOutlet UILabel *labelNine;
+@property (strong, nonatomic) IBOutlet UILabel *whichPlayerLabel;
 @end
 
 @implementation ViewController
@@ -19,9 +28,12 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)onLabelTapped:(UITapGestureRecognizer *)tapGesture {
+
+}
+
+- (void) findLabelUsingPoint:(CGPoint) point {
+    
 }
 
 @end
