a8d641f87a00b7ab1fe19555d2ffd209986afebe
B2MW <P5@Champion.local>
2014-10-02 16:19:34 -0500

    Initial Commit

---
+++ TicTacToe/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TicTacToe
+//
+//  Created by Bradley Walker on 10/2/14.
+//  Copyright (c) 2014 BlackSummerVentures. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TicTacToe/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TicTacToe
+//
+//  Created by Bradley Walker on 10/2/14.
+//  Copyright (c) 2014 BlackSummerVentures. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
