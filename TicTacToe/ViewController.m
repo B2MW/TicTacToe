58c092f8a774c08291b15ffa0927c74cc98eddad
B2MW <P5@Champion.local>
2014-10-02 18:25:05 -0500

    Alternate method for finding label and tap intersections

--- TicTacToe/ViewController.m
+++ TicTacToe/ViewController.m
@@ -8,7 +8,7 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UIGestureRecognizerDelegate>
 @property (strong, nonatomic) IBOutlet UILabel *labelOne;
 @property (strong, nonatomic) IBOutlet UILabel *labelTwo;
 @property (strong, nonatomic) IBOutlet UILabel *labelThree;
@@ -18,22 +18,40 @@
 @property (strong, nonatomic) IBOutlet UILabel *labelSeven;
 @property (strong, nonatomic) IBOutlet UILabel *labelEight;
 @property (strong, nonatomic) IBOutlet UILabel *labelNine;
+
 @property (strong, nonatomic) IBOutlet UILabel *whichPlayerLabel;
+@property NSMutableArray *labelFrames;
+
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    [self.labelFrames addObject:[NSValue valueWithCGRect:self.labelOne.frame]];
+    [self.labelFrames addObject:[NSValue valueWithCGRect:self.labelTwo.frame]];
+    [self.labelFrames addObject:[NSValue valueWithCGRect:self.labelThree.frame]];
+    [self.labelFrames addObject:[NSValue valueWithCGRect:self.labelFour.frame]];
+    [self.labelFrames addObject:[NSValue valueWithCGRect:self.labelFive.frame]];
+    [self.labelFrames addObject:[NSValue valueWithCGRect:self.labelSix.frame]];
+    [self.labelFrames addObject:[NSValue valueWithCGRect:self.labelSeven.frame]];
+    [self.labelFrames addObject:[NSValue valueWithCGRect:self.labelEight.frame]];
+    [self.labelFrames addObject:[NSValue valueWithCGRect:self.labelNine.frame]];
 }
 
 - (IBAction)onLabelTapped:(UITapGestureRecognizer *)tapGesture {
-
+    if (tapGesture.state == UIGestureRecognizerStateBegan) {
+        CGPoint point = [tapGesture locationInView:self.view];
+    }
 }
 
 - (void) findLabelUsingPoint:(CGPoint) point {
-    
+    for (int i = 0; i <= self.labelFrames.count; i++) {
+        CGRect *currentRect = (__bridge CGRect *)([self.labelFrames objectAtIndex:i]);
+        if (CGRectContainsPoint(*currentRect, point)) {
+            NSLog(@"Label and point intersect");
+        } else NSLog(@"Lable and point do NOT intersect");
+    }
 }
 
 @end
